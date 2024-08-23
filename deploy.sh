1c6d2aaff3f38258a2e7a63b0155c3ab0ded15ed
JoonHoSeong <joonho1366@gmail.com>
2024-08-24 03:15:03 +0900

    [Fix] CI/CD Code14

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# 1. 작업 디렉토리 설정
-cd oz_03_collabo-003-BE  # Django 프로젝트가 위치한 디렉토리로 변경
+DOCKER_IMAGE_NAME=$1  # Docker 이미지 이름 받아오기
 
-# 2. 필요한 경우 Docker 이미지 pull (선택 사항)
-# docker-compose pull
-
-# 3. 기존 컨테이너 중지 및 삭제
+# 1. 기존 컨테이너 중지 및 삭제
 docker-compose down
+
+# 2. Docker 이미지 pull (선택 사항)
+# docker pull $DOCKER_IMAGE_NAME  # 필요한 경우 Docker 이미지를 레지스트리에서 pull
+
+# 3. Docker 이미지 태그 변경 (선택 사항)
+# docker tag $DOCKER_IMAGE_NAME your-image-name:latest  # 필요한 경우 Docker 이미지 태그 변경
 
 # 4. 마이그레이션 실행
 docker-compose run --rm web python manage.py migrate
