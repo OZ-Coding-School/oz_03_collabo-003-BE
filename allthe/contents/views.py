7967ae9c596e6146f8237a7351650323ed6f4fc7
LIM0310 <limsy0310@gmail.com>
2024-08-25 04:03:41 +0900

    add uploadcontent

--- allthe/contents/views.py
+++ allthe/contents/views.py
@@ -6,29 +6,39 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .models import Content
 from .models import Image
 from .serializers import ContentsSerializer
 
 
-# 콘텐츠 업로드 api
+# 콘텐츠 업로드(post), 모든 콘텐츠 list(get)
 class UploadContent(APIView):
     permission_classes = [IsAuthenticated]  # 인증된 사용자만 접근 허용
 
     @swagger_auto_schema(
-        request_body=ContentsSerializer,
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "title": openapi.Schema(type=openapi.TYPE_STRING),
+                "content": openapi.Schema(type=openapi.TYPE_STRING),
+                "category": openapi.Schema(type=openapi.TYPE_STRING),
+                "site_url": openapi.Schema(type=openapi.TYPE_STRING),
+                "site_description": openapi.Schema(
+                    type=openapi.TYPE_STRING, default=""
+                ),
+                "is_analyzed": openapi.Schema(type=openapi.TYPE_BOOLEAN, default=False),
+                "images": openapi.Schema(
+                    type=openapi.TYPE_ARRAY,
+                    items=openapi.Schema(type=openapi.TYPE_FILE),
+                    description="Upload multiple images",
+                    default=[],
+                ),
+            },
+        ),
         responses={
             status.HTTP_201_CREATED: ContentsSerializer,
             status.HTTP_400_BAD_REQUEST: openapi.Response("Bad Request"),
         },
-        manual_parameters=[
-            openapi.Parameter(
-                name="images",
-                in_=openapi.IN_FORM,
-                type=openapi.TYPE_FILE,
-                description="Upload multiple images",
-                required=False,
-            ),
-        ],
         operation_summary="Upload new content with images",
         operation_description="This endpoint allows users to upload new content with multiple images. Only non-analyst users can upload content.",
     )
@@ -61,3 +71,17 @@ class UploadContent(APIView):
             return Response(content_serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(content_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @swagger_auto_schema(
+        responses={
+            status.HTTP_200_OK: ContentsSerializer(many=True),
+            status.HTTP_401_UNAUTHORIZED: openapi.Response("Unauthorized"),
+        },
+        operation_summary="List all content",
+        operation_description="Retrieve a list of all content objects. Only authenticated users can access this endpoint.",
+    )
+    def get(self, request):
+        # 모든 콘텐츠 객체 가져오기
+        contents = Content.objects.all()
+        serializer = ContentsSerializer(contents, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
